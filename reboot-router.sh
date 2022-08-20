c5f271c637dc47f844858b49e4e7c4f6b5969106
atrox3d <atrox3d@gmail.com>
2022-08-20 11:38:49 +0200

    log

--- reboot-router.sh
+++ reboot-router.sh
@@ -5,6 +5,6 @@ HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"	# get current path
 HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"	# get current path
 NAME="$(basename ${BASH_SOURCE[0]})"							# save this script name
 #########################################################################################
-
-info "RUN | " "${HERE}"/ssh-server.sh -f "$(basename ${0} .sh).log" reboot now
-"${HERE}"/ssh-server.sh -l "$(basename ${0} .sh).log" reboot now
+LOG_FILE="$(basename ${0} .sh).log"
+info "RUN | " "${HERE}"/ssh-server.sh -f "${LOG_FILE}" reboot now | tee -a  "${LOG_FILE}" 
+"${HERE}"/ssh-server.sh -l "${LOG_FILE}" reboot now
