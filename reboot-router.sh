9434d32ff6324d28d47b350e99597be94e9b2d2e
atrox3d <atrox3d@gmail.com>
2022-08-20 10:39:45 +0200

    add logfile name

--- reboot-router.sh
+++ reboot-router.sh
@@ -6,5 +6,5 @@ HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"	# get current path
 NAME="$(basename ${BASH_SOURCE[0]})"							# save this script name
 #########################################################################################
 
-info "RUN | " "${HERE}"/ssh-server.sh reboot now
-"${HERE}"/ssh-server.sh reboot now
+info "RUN | " "${HERE}"/ssh-server.sh -f "$(basename ${0} .sh).log" reboot now
+"${HERE}"/ssh-server.sh -l "$(basename ${0} .sh).log" reboot now
