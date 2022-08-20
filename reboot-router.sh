ff427fff6297a2075ff9b2a526fb271d66c6292e
atrox3d <atrox3d@gmail.com>
2022-08-20 07:57:28 +0200

    add .setup call

--- reboot-router.sh
+++ reboot-router.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#########################################################################################
 HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"	# get current path
+. "${HERE}/.setup"												# load modules
+HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"	# get current path
+NAME="$(basename ${BASH_SOURCE[0]})"							# save this script name
+#########################################################################################
 
 "${HERE}"/ssh-server.sh reboot now
--- ssh-server.sh
+++ ssh-server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+#########################################################################################
 HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"	# get current path
+. "${HERE}/.setup"												# load modules
+HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"	# get current path
+NAME="$(basename ${BASH_SOURCE[0]})"							# save this script name
+#########################################################################################
 CONFIG_FILE=${HERE}/private/config
 
 function get_key()
