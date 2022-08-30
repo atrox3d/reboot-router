85bd3e2b2af9150271d70745e6bd5761ebd380c8
atrox3d <atrox3d@gmail.com>
2022-08-30 08:53:05 +0200

    fix log path

--- reboot-router.sh
+++ reboot-router.sh
@@ -7,12 +7,14 @@ NAME="$(basename ${BASH_SOURCE[0]})"							# save this script name
 #########################################################################################
 
 LOG_FILE="$(basename ${0} .sh).log"
+# info "LOGFILE         | ${HERE}/${LOG_FILE}"
+# info "LOGFILE SUMMARY | ${HERE}/${LOG_FILE}.summary"
 info "RUN | " "${HERE}"/ssh-server.sh -l "${LOG_FILE}" reboot now |& tee -a  "${HERE}/${LOG_FILE}" |& tee -a "${HERE}/${LOG_FILE}.summary"
-"${HERE}"/ssh-server.sh -l "${LOG_FILE}" reboot now
+#"${HERE}"/ssh-server.sh -l "${LOG_FILE}" reboot now
 
 if [ $? -eq 0 ]
 then
-	info "STATUS | SUCCESS" |& tee -a "${LOG_FILE}.summary"
+	info "STATUS | SUCCESS" |& tee -a "${HERE}/${LOG_FILE}.summary"
 else
-	info "STATUS | ERROR" |& tee -a "${LOG_FILE}.summary"
+	info "STATUS | ERROR" |& tee -a "${HERE}/${LOG_FILE}.summary"
 fi
