7f634af4a956b380091b574852048a9f6ef3367b
atrox3d <atrox3d@gmail.com>
2022-10-27 13:58:18 +0200

    uncomment execution lines

--- reboot-router.sh
+++ reboot-router.sh
@@ -7,10 +7,10 @@ NAME="$(basename ${BASH_SOURCE[0]})"							# save this script name
 #########################################################################################
 
 LOG_FILE="$(basename ${0} .sh).log"
-# info "LOGFILE         | ${HERE}/${LOG_FILE}"
-# info "LOGFILE SUMMARY | ${HERE}/${LOG_FILE}.summary"
+info "LOGFILE         | ${HERE}/${LOG_FILE}"
+info "LOGFILE SUMMARY | ${HERE}/${LOG_FILE}.summary"
 info "RUN | " "${HERE}"/ssh-server.sh -l "${LOG_FILE}" reboot now |& tee -a  "${HERE}/${LOG_FILE}" |& tee -a "${HERE}/${LOG_FILE}.summary"
-#"${HERE}"/ssh-server.sh -l "${LOG_FILE}" reboot now
+"${HERE}"/ssh-server.sh -l "${LOG_FILE}" reboot now
 
 if [ $? -eq 0 ]
 then
